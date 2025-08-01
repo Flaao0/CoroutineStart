bedfd6ebc2f74cd7f4ec73f7999d223cf7d1492a
Flaao0 <v.petunin06@mail.ru>
2025-08-01 14:02:11 +0300

    Initial commit

---
+++ app/src/main/java/com/flaao0/coroutinestart/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.flaao0.coroutinestart
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.flaao0.coroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        loadData()
+    }
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity {city ->
+            binding.tvLocation.text = city
+            loadTemperature(city) {temp ->
+                binding.tvTemperature.text = temp.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback("Moscow")
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+
+        thread {
+            Toast.makeText(
+                this,
+                getString(R.string.loading_temperature_toast, city),
+                Toast.LENGTH_SHORT
+            ).show()
+            Thread.sleep(5000)
+            callback(17)
+        }
+    }
+}
