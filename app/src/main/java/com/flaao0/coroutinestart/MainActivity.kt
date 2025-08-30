78faf51b1d02414f0f2964c52d74a68070be3488
Flaao0 <v.petunin06@mail.ru>
2025-08-30 22:16:43 +0300

    use async

--- app/src/main/java/com/flaao0/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/flaao0/coroutinestart/MainActivity.kt
@@ -9,9 +9,10 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.flaao0.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,17 +35,24 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             binding.progress.isVisible = true
             binding.buttonLoad.isEnabled = false
-            val jobCity = lifecycleScope.launch {
-                val city = loadCity()
-                binding.tvLocation.text = city
+            val deferredCity: Deferred<String> = lifecycleScope.async {
+               loadCity()
             }
-            val jobTemp = lifecycleScope.launch {
-                val temp = loadTemperature()
-                binding.tvTemperature.text = temp.toString()
+
+            val deferredTemp: Deferred<Int> = lifecycleScope.async {
+                loadTemperature()
             }
+
             lifecycleScope.launch {
-                jobCity.join()
-                jobTemp.join()
+                val city = deferredCity.await()
+                val temp = deferredTemp.await()
+                binding.tvLocation.text = city
+                binding.tvTemperature.text = temp.toString()
+                Toast.makeText(
+                    this@MainActivity,
+                    "City: $city, Temp: $temp",
+                    Toast.LENGTH_SHORT
+                ).show()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
             }
